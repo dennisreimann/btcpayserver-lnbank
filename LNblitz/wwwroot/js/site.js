dc867fece63354b1077150072688e80462401f74
Dennis Reimann <mail@dennisreimann.de>
2020-06-18 17:53:22 +0200

    Apply design basics

--- LNblitz/wwwroot/js/site.js
+++ LNblitz/wwwroot/js/site.js
@@ -1,4 +1,25 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿// Theme Switch
+const COLOR_MODES = ["light", "dark"];
+const THEME_ATTR = "data-btcpay-theme";
+const STORE_ATTR = "btcpay-theme";
+const systemColorMode = window.matchMedia("(prefers-color-scheme: dark)").matches ? COLOR_MODES[1] : COLOR_MODES[0];
+const userColorMode = window.localStorage.getItem(STORE_ATTR);
+const initialColorMode = COLOR_MODES.includes(userColorMode) ? userColorMode : systemColorMode;
 
-// Write your JavaScript code.
+function setColorMode (mode) {
+    if (COLOR_MODES.includes(mode)) {
+        window.localStorage.setItem(STORE_ATTR, mode);
+        document.documentElement.setAttribute(THEME_ATTR, mode);
+    }
+};
+
+setColorMode(initialColorMode);
+
+document.querySelectorAll(".btcpay-theme-switch").forEach(function (link) {
+    link.addEventListener("click", function (e) {
+        e.preventDefault();
+        const current = document.documentElement.getAttribute(THEME_ATTR) || COLOR_MODES[0];
+        const mode = current === COLOR_MODES[0] ? COLOR_MODES[1] : COLOR_MODES[0];
+        setColorMode(mode);
+    });
+});
