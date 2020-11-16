173d04755d06c71da500abf36040817044801ce2
Dennis Reimann <mail@dennisreimann.de>
2020-11-16 17:06:29 +0100

    Move themeing js to head
    Prevents flash of unstyled content

--- LNbank/wwwroot/js/site.js
+++ LNbank/wwwroot/js/site.js
@@ -1,21 +1,4 @@
-﻿"use strict";
-
-// Theme Switch
-const COLOR_MODES = ["light", "dark"]
-const THEME_ATTR = "data-btcpay-theme"
-const STORE_ATTR = "btcpay-theme"
-const systemColorMode = window.matchMedia("(prefers-color-scheme: dark)").matches ? COLOR_MODES[1] : COLOR_MODES[0]
-const userColorMode = window.localStorage.getItem(STORE_ATTR)
-const initialColorMode = COLOR_MODES.includes(userColorMode) ? userColorMode : systemColorMode
-
-const setColorMode = mode => {
-    if (COLOR_MODES.includes(mode)) {
-        window.localStorage.setItem(STORE_ATTR, mode)
-        document.documentElement.setAttribute(THEME_ATTR, mode)
-    }
-}
-
-setColorMode(initialColorMode)
+﻿"use strict"
 
 // Clipboard
 window.copyToClipboard = (e, text) => {
